1de35e07fd998d66f7ead6a62bf6319aa8ae33d5
tjeveraldo <36521432+tjeveraldo@users.noreply.github.com>
2018-04-17 12:30:16 -0300

    Exercicio Java 12/04/2018

--- Questao1.java
+++ Questao1.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
  * @author everaldo.gomes
  *
  */
+
 public class Questao1 {
 	public static void main(String[] args) {
 		int[] array = new int[300];
@@ -60,7 +61,72 @@ public class Questao1 {
 		}
 
 		}
-	}
+//questao5
 	
+	private int TAM_LISTA = 10;
+	private int[] lista = new int[TAM_LISTA];
+	Scanner input = new Scanner(System.in);
 
+/**
+ * Método para receber os valores
+ */
+	
+	public void inputValores() {
+		System.out.printf("Informe 10 valores em uma única linha: ");
+		for(int i = 0; i < lista.length; i++) {
+			lista[i] = input.nextInt();
+		}
+		
+		this.ordenaListaCrescente();
+	}
 
+/**
+ * Método para ordenar a lista de forma crescente
+ */
+	
+	private void ordenaListaCrescente() {
+		int temp = 0;
+		
+		for(int i = 0; i < lista.length; i++) { 
+			for(int j = i; j < lista.length; j++) {
+				if(this.lista[i] > lista[j]) {
+					temp = lista[i];
+					lista[i] = lista[j];
+					lista[j] = temp;
+				}
+			}
+		}	
+	}
+
+/**
+ * Método para imprimir a lista 
+ */
+	
+	public void imprimeListaOrdenada() {
+		for(int i = 0; i < lista.length; i++)
+			System.out.printf("[%d] = %d%n", i, lista[i]);
+	}
+//questao6
+	private String cpf;
+	private String nome;
+		{
+		 this.cpf = n_cpf;
+		 this.nome = n_nome;
+		}
+
+		public void setCpf(String n_cpf) {
+			cpf = n_cpf;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public void setNome(String n_nome) {
+			nome = n_nome;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+}
